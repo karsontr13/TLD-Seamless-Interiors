b1708d64e77814c43397b1025d96cc269b084328
karsontr13 <karsontr13@gmail.com>
2026-07-01 19:55:16 +0300

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(CampOfficeOverhaul.CampOfficeMod), "Camp Office Overhaul", "Deneme", "HamsiBuglama")]
+[assembly: MelonInfo(typeof(CampOfficeOverhaul.CampOfficeMod), "Seamless-CampOffice", "Deneme", "HamsiBuglama")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace CampOfficeOverhaul
